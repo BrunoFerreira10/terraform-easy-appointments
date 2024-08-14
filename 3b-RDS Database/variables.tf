bf30726259531494c4c0ddbbb6132d9da7a5e793
BrunoFerreira10 <brunoferreira86dev@gmail.com>
2024-08-14 14:11:56 -0300

    Fix

--- 3b-RDS Database/variables.tf
+++ 3b-RDS Database/variables.tf
@@ -1,24 +1,46 @@
-variable "regiao" {
+#### Start default project github vars ####
+variable "GENERAL_TAG_PROJECT" {
+  type        = string
+  description = "Descrição do projeto."
+}
+
+variable "GENERAL_TAG_CUSTOMER" {
+  type        = string
+  description = "Cliente do projeto."
+}
+
+variable "GENERAL_TAG_AUTHOR" {
+  type        = string
+  description = "Autor de edição."
+}
+
+variable "GENERAL_TAG_SHORTNAME" {
+  type        = string
+  description = "Nome curto para identificação dos recursos na AWS"
+}
+
+variable "GENERAL_REGION" {
   type        = string
   description = "Região onde a infraestrutura será criada."
 }
 
-variable "remote-state-bucket" {
+variable "GENERAL_REMOTE_STATE_BUCKET" {
   type        = string
   description = "Bucket name onde está o remote state"
 }
+#### End default project github vars ####
 
-variable "rds-1-db-name" {
+variable "RDS_1_DB_NAME" {
   type        = string
   description = "DB Name do RDS 1"
 }
 
-variable "rds-1-db-username" {
+variable "RDS_1_DB_USERNAME" {
   type        = string
   description = "DB User name do RDS 1"
 }
 
-variable "rds-1-db-password" {
+variable "RDS_1_DB_PASSWORD" {
   type        = string
   description = "DB Password do RDS 1"
   sensitive   = true

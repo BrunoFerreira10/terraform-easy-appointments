07b206f29accc218ce8b6f0346bf1a1445eafbcb
BrunoFerreira10 <brunoferreira86dev@gmail.com>
2024-08-14 15:15:38 -0300

    Fix

--- 4c-Image Builder/image-builder-1/08-pipeline.tf
+++ 4c-Image Builder/image-builder-1/08-pipeline.tf
@@ -5,7 +5,7 @@ resource "aws_imagebuilder_image_pipeline" "img-builder-pipeline-1" {
   infrastructure_configuration_arn = aws_imagebuilder_infrastructure_configuration.infrastructure-1.arn
   distribution_configuration_arn   = aws_imagebuilder_distribution_configuration.distribution-1.arn
 
-  execution_role = "arn:aws:iam::096320584768:role/aws-service-role/imagebuilder.amazonaws.com/AWSServiceRoleForImageBuilder"
+  execution_role = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/aws-service-role/imagebuilder.amazonaws.com/AWSServiceRoleForImageBuilder"
   workflow {
     workflow_arn = aws_imagebuilder_workflow.img-builder-workflow-1.arn
   }

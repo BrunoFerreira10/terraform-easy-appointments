95c1c3c489b6ba614a6055f8c70b75f7d3ba68e2
BrunoFerreira10 <brunoferreira86dev@gmail.com>
2024-08-14 15:02:41 -0300

    Fix

--- 4c-Image Builder/image-builder-1/05b-image-recipe-configuration.tf
+++ 4c-Image Builder/image-builder-1/05b-image-recipe-configuration.tf
@@ -1,7 +1,7 @@
 resource "aws_imagebuilder_image_recipe" "recipe-2" {
   name         = "${var.shortname}-recipe-2"
   version      = "1.0.0"
-  parent_image = var.IMAGE_BUILDER_PARENT_IMAGE
+  parent_image = tolist(aws_imagebuilder_image.image-1.output_resources[0].amis)[0].image
 
   component {
     component_arn = aws_imagebuilder_component.apply-github-ssh-private-key.arn
--- 4c-Image Builder/image-builder-1/09b-image-configuration.tf
+++ 4c-Image Builder/image-builder-1/09b-image-configuration.tf
@@ -3,7 +3,7 @@ resource "aws_imagebuilder_image" "image-2" {
   image_recipe_arn                 = aws_imagebuilder_image_recipe.recipe-2.arn
   infrastructure_configuration_arn = aws_imagebuilder_infrastructure_configuration.infrastructure-1.arn
 
-  execution_role = "arn:aws:iam::096320584768:role/aws-service-role/imagebuilder.amazonaws.com/AWSServiceRoleForImageBuilder"
+  execution_role = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/aws-service-role/imagebuilder.amazonaws.com/AWSServiceRoleForImageBuilder"
   workflow {
     workflow_arn = aws_imagebuilder_workflow.img-builder-workflow-1.arn
   }

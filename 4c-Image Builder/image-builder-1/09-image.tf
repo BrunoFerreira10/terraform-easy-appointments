aec83f3d1dad67ce6e37086cb055889906bb0f7d
BrunoFerreira10 <brunoferreira86dev@gmail.com>
2024-08-15 07:31:10 -0300

    Fix

---
+++ 4c-Image Builder/image-builder-1/09-image.tf
@@ -0,0 +1,32 @@
+resource "aws_imagebuilder_image" "image-1" {
+  distribution_configuration_arn   = aws_imagebuilder_distribution_configuration.distribution-1.arn
+  image_recipe_arn                 = aws_imagebuilder_image_recipe.recipe-1.arn
+  infrastructure_configuration_arn = aws_imagebuilder_infrastructure_configuration.infrastructure-1.arn
+
+  execution_role = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/aws-service-role/imagebuilder.amazonaws.com/AWSServiceRoleForImageBuilder"
+  workflow {
+    workflow_arn = aws_imagebuilder_workflow.img-builder-workflow-1.arn
+  }
+}
+
+# resource "aws_imagebuilder_image" "image-2" {
+#   distribution_configuration_arn   = aws_imagebuilder_distribution_configuration.distribution-2.arn
+#   image_recipe_arn                 = aws_imagebuilder_image_recipe.recipe-2.arn
+#   infrastructure_configuration_arn = aws_imagebuilder_infrastructure_configuration.infrastructure-1.arn
+
+#   execution_role = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/aws-service-role/imagebuilder.amazonaws.com/AWSServiceRoleForImageBuilder"
+#   workflow {
+#     workflow_arn = aws_imagebuilder_workflow.img-builder-workflow-1.arn
+#   }
+# }
+
+resource "aws_imagebuilder_image" "image-3" {
+  distribution_configuration_arn   = aws_imagebuilder_distribution_configuration.distribution-3.arn
+  image_recipe_arn                 = aws_imagebuilder_image_recipe.recipe-3.arn
+  infrastructure_configuration_arn = aws_imagebuilder_infrastructure_configuration.infrastructure-1.arn
+
+  execution_role = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/aws-service-role/imagebuilder.amazonaws.com/AWSServiceRoleForImageBuilder"
+  workflow {
+    workflow_arn = aws_imagebuilder_workflow.img-builder-workflow-1.arn
+  }
+}

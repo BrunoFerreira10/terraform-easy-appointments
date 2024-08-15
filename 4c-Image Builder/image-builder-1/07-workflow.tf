18f85d28cff3fbae57a9973673f41ae281a673d9
BrunoFerreira10 <brunoferreira86dev@gmail.com>
2024-08-15 15:04:30 -0300

    Fix

--- 4c-Image Builder/image-builder-1/07-workflow.tf
+++ 4c-Image Builder/image-builder-1/07-workflow.tf
@@ -3,6 +3,10 @@ resource "aws_imagebuilder_workflow" "img-builder-workflow-1" {
   version = "1.0.0"
   type    = "BUILD"
 
+  lifecycle {
+    ignore_changes = [data]
+  }
+
   data = templatefile("${path.module}/workflows/workflow-1.tpl", {
   })
 }

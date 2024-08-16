f079a929b7eafdf79392a97c02d0df5e8bd09831
BrunoFerreira10 <brunoferreira86dev@gmail.com>
2024-08-16 14:00:44 -0300

    Fix

--- 4e-Code Pipeline/code-pipeline/variables.tf
+++ 4e-Code Pipeline/code-pipeline/variables.tf
@@ -14,13 +14,3 @@ variable "GENERAL_PROJECT_BUCKET" {
   description = "Bucket name onde está o remote state"
 }
 #### End default project github vars ####
-
-variable "RDS_1_DB_NAME" {
-  type        = string
-  description = "DB Name do RDS 1"
-}
-
-variable "RDS_1_DB_USERNAME" {
-  type        = string
-  description = "DB User name do RDS 1"
-}

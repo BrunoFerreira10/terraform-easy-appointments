b667603438897b91691aff6b3062089beaee0553
BrunoFerreira10 <brunoferreira86dev@gmail.com>
2024-08-21 19:44:13 -0300

    Corrigidos cidr_blocks

--- modules/compute/ec2/generic_elb/01_security_groups.tf
+++ modules/compute/ec2/generic_elb/01_security_groups.tf
@@ -5,15 +5,15 @@ locals {
   sg_elb_setup = {
     ingress = {
       HTTP = {
-        port = 80, cidr_blocks = ["0.0.0.0/0"]
+        port = 80, cidr_blocks = "0.0.0.0/0"
       },
       HTTPS = {
-        port = 443, cidr_blocks = ["0.0.0.0/0"]
+        port = 443, cidr_blocks = "0.0.0.0/0"
       }
     },
     egress = {
       All = {
-        port = 0, cidr_blocks = ["0.0.0.0/0"],
+        port = 0, cidr_blocks = "0.0.0.0/0",
         description = "Allow all traffic"
       }
     }
@@ -35,7 +35,7 @@ resource "aws_vpc_security_group_ingress_rule" "elb" {
 
   description       = lookup(each.value,"description","Allow ${each.key}")
   security_group_id = aws_security_group.elb.id
-  cidr_ipv4         = lookup(each.value,"cidr_blocks",[var.vpc.cidr_block])
+  cidr_ipv4         = lookup(each.value,"cidr_blocks",var.vpc.cidr_block)
   ip_protocol       = lookup(each.value,"protocol","tcp")
   from_port         = lookup(each.value,"from_port",each.value.port)
   to_port           = lookup(each.value,"to_port",each.value.port)
@@ -46,7 +46,7 @@ resource "aws_vpc_security_group_egress_rule" "elb" {
 
   description       = lookup(each.value,"description","Allow ${each.key}")
   security_group_id = aws_security_group.elb.id
-  cidr_ipv4         = lookup(each.value,"cidr_blocks",[var.vpc.cidr_block])
+  cidr_ipv4         = lookup(each.value,"cidr_blocks",var.vpc.cidr_block)
   ip_protocol       = lookup(each.value,"protocol","tcp")
   from_port         = lookup(each.value,"from_port",each.value.port)
   to_port           = lookup(each.value,"to_port",each.value.port)

35c0f85b6a52742e9642c5cffc3b4e85a551ed04
BrunoFerreira10 <brunoferreira86dev@gmail.com>
2024-08-21 09:32:48 -0300

    Adicionado ssm_parameter_db_password ao output

--- modules/database/rds/generic-mysql/outputs.tf
+++ modules/database/rds/generic-mysql/outputs.tf
@@ -1,9 +1,12 @@
 output "rds" {
   description = "RDS MySQL Information"
   value = {
-    id       = aws_db_instance.rds.id,
-    arn      = aws_db_instance.rds.arn,
-    endpoint = split(":", aws_db_instance.rds.endpoint)[0],
-    port     = aws_db_instance.rds.port
+    id                        = aws_db_instance.rds.id,
+    arn                       = aws_db_instance.rds.arn,
+    endpoint                  = split(":", aws_db_instance.rds.endpoint)[0],
+    port                      = aws_db_instance.rds.port,
+    db_name                   = aws_db_instance.rds.db_name
+    db_username               = aws_db_instance.rds.username
+    ssm_parameter_db_password = var.rds_configuration.ssm_parameter_db_password
   }
 }

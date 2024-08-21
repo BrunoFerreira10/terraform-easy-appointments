6c9fb35300662cac6bcc5c0fa2e4ab95045c70c4
BrunoFerreira10 <brunoferreira86dev@gmail.com>
2024-08-21 10:01:30 -0300

    Corrigido cidr_ipv4

--- modules/storage/efs/generic_efs/01_security_group.tf
+++ modules/storage/efs/generic_efs/01_security_group.tf
@@ -14,7 +14,7 @@ resource "aws_security_group" "efs_mountpoints" {
 ## Igress Rules
 resource "aws_vpc_security_group_ingress_rule" "allow_efs_access" {
   security_group_id = aws_security_group.efs_mountpoints.id
-  cidr_ipv4         = aws_vpc.vpc.cidr_blocks // Same VPC only
+  cidr_ipv4         = var.vpc.cidr_blocks // Same VPC only
   ip_protocol       = "tcp"
   from_port         = 2049
   to_port           = 2049
@@ -23,7 +23,7 @@ resource "aws_vpc_security_group_ingress_rule" "allow_efs_access" {
 ## Egress Rules
 resource "aws_vpc_security_group_egress_rule" "allow_output_traffic" {
   security_group_id = aws_security_group.efs_mountpoints.id
-  cidr_ipv4         = aws_vpc.vpc.cidr_blocks // Same VPC only
+  cidr_ipv4         = var.vpc.cidr_blocks // Same VPC only
   ip_protocol       = "-1"
 }
 ## --------------------------------------------------------------------------------------------------------------------

ba72dedcd90e86d6a51fb8ada3b360771354fd8c
BrunoFerreira10 <brunoferreira86dev@gmail.com>
2024-08-22 19:15:21 -0300

    Refatorado security group do bastion

--- modules/compute/ec2/bastion/variables.tf
+++ modules/compute/ec2/bastion/variables.tf
@@ -19,22 +19,26 @@ variable "vpc" {
 
 variable "sg_bastion_rules" {
   description = "Rules for bastion host security group"
-  type        = map(object({
-    ingress = map(object({
-      description = optional(string),
-      cidr_ipv4   = optional(string),
-      ip_protocol = optional(string, "tcp"),
-      from_port   = optional(number),
-      to_port     = optional(number),
-      port        = optional(number)
-    })),
-    egress = map(object({
-      description = optional(string),
-      cidr_ipv4   = optional(string),
-      ip_protocol = optional(string, "tcp"),
-      from_port   = optional(number),
-      to_port     = optional(number),
-      port        = optional(number)
-    }))
-  }))
-}
+  type        = any
+
+# variable "sg_bastion_rules" {
+#   description = "Rules for bastion host security group"
+#   type        = map(object({
+#     ingress = map(object({
+#       description = optional(string),
+#       cidr_ipv4   = optional(string),
+#       ip_protocol = optional(string, "tcp"),
+#       from_port   = optional(number),
+#       to_port     = optional(number),
+#       port        = optional(number)
+#     })),
+#     egress = map(object({
+#       description = optional(string),
+#       cidr_ipv4   = optional(string),
+#       ip_protocol = optional(string, "tcp"),
+#       from_port   = optional(number),
+#       to_port     = optional(number),
+#       port        = optional(number)
+#     }))
+#   }))
+# }

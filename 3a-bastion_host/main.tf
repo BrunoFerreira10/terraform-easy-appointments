6769cd7a94a3145c13d7bc38528285ec755962d8
BrunoFerreira10 <brunoferreira86dev@gmail.com>
2024-08-22 17:36:48 -0300

    Adicionada entrada shortname

--- 3a-bastion_host/main.tf
+++ 3a-bastion_host/main.tf
@@ -1,6 +1,7 @@
 module "bastion" {
   source               = "../modules/compute/ec2/bastion"
-  region               = module.data.github_vars.general_region
   ec2_ssh_keypair_name = module.data.github_vars.ec2_ssh_keypair_name
+  region               = module.data.github_vars.general_region
+  shortname            = module.data.github_vars.general_tag_shortname
   vpc                  = module.data.projects.vpc_app.vpc
 }

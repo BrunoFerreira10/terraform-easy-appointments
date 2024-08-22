c15ae1ca05db1f4b27615bc146dac8a8296c432c
BrunoFerreira10 <brunoferreira86dev@gmail.com>
2024-08-21 23:08:36 -0300

    Teste sec. groups

--- modules/compute/ec2/bastion/02_security_groups.tf
+++ modules/compute/ec2/bastion/02_security_groups.tf
@@ -13,6 +13,14 @@ resource "aws_security_group" "bastion" {
 
   egress {
     description = "Allow HTTP"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "TCP"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    description = "Allow HTTP"
     from_port   = 80
     to_port     = 80
     protocol    = "TCP"

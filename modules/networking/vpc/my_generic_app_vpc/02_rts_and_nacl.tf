19bee6c6ef49c31fa9c9a79a145ce79985d6607a
BrunoFerreira10 <brunoferreira86dev@gmail.com>
2024-08-21 22:00:09 -0300

    Corrigido security groups

--- modules/networking/vpc/my_generic_app_vpc/02_rts_and_nacl.tf
+++ modules/networking/vpc/my_generic_app_vpc/02_rts_and_nacl.tf
@@ -69,7 +69,8 @@ locals {
     { rule_number = 100, egress = true, cidr = "0.0.0.0/0", port = 22 },
     { rule_number = 200, egress = true, cidr = "0.0.0.0/0", port = 80 },
     { rule_number = 300, egress = true, cidr = "0.0.0.0/0", port = 443 },
-    { rule_number = 400, egress = true, cidr = "0.0.0.0/0", from_port = 32768, to_port = 65535 }
+    { rule_number = 400, egress = true, cidr = "0.0.0.0/0", port = 3306 },
+    { rule_number = 500, egress = true, cidr = "0.0.0.0/0", from_port = 32768, to_port = 65535 }
   ]
   private_nacl_rules = [
     { rule_number = 100, egress = false, cidr = aws_vpc.app.cidr_block, port = 22 },
@@ -77,7 +78,7 @@ locals {
     { rule_number = 300, egress = false, cidr = "0.0.0.0/0", port = 443 },
     { rule_number = 400, egress = false, cidr = "0.0.0.0/0", port = 3306 },
     { rule_number = 500, egress = false, cidr = "0.0.0.0/0", from_port = 32768, to_port = 65535 },
-    { rule_number = 100, egress = true, cidr = "0.0.0.0/0", from_port = 32768, to_port = 65535 },
+    { rule_number = 100, egress = true, cidr = "0.0.0.0/0", from_port = 1024, to_port = 65535 },
   ]
 }
 

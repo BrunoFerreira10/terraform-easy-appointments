31d7398eb4bb4e6d57a33fd1ba3dd703e9e8015c
BrunoFerreira10 <brunoferreira86dev@gmail.com>
2024-08-22 19:39:12 -0300

    Refatorado security group do bastion

--- modules/database/rds/generic-mysql/02_rds.tf
+++ modules/database/rds/generic-mysql/02_rds.tf
@@ -25,7 +25,7 @@ resource "aws_db_instance" "rds" {
   publicly_accessible = var.rds_configuration.publicly_accessible
 
   vpc_security_group_ids = [
-    module.generic_security_group.security_group.id
+    module.sg_rds.security_group.id
   ]
 
   tags = {

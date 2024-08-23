04b63d531aeac69a5fd985298401e3f42de85b4b
BrunoFerreira10 <brunoferreira86dev@gmail.com>
2024-08-23 14:07:02 -0300

    Launch template v3

--- modules/compute/ec2/generic_elb/07_asg.tf
+++ modules/compute/ec2/generic_elb/07_asg.tf
@@ -16,7 +16,7 @@ resource "aws_autoscaling_group" "this" {
   // Launch template
   launch_template {
     id      = aws_launch_template.this.id
-    version = "2"
+    version = "3"
   }
 
   // Instance refresh

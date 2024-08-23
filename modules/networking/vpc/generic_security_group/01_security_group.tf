3cd83ee498b02061c1cd3619d762942d078b8b5a
BrunoFerreira10 <brunoferreira86dev@gmail.com>
2024-08-23 08:52:34 -0300

    Corrigido cidr_ipv4

--- modules/networking/vpc/generic_security_group/01_security_group.tf
+++ modules/networking/vpc/generic_security_group/01_security_group.tf
@@ -17,7 +17,7 @@ resource "aws_vpc_security_group_ingress_rule" "this" {
 
   security_group_id = aws_security_group.this.id
   description       = each.value.description != null ? each.value.description : "Allow ${each.key}"
-  cidr_ipv4         = each.value.cidr_ipv4 != null ? each.value.cidr_ipv4 : "tcp"
+  cidr_ipv4         = each.value.cidr_ipv4 != null ? each.value.cidr_ipv4 : var.vpc.cidr_block
   ip_protocol       = each.value.ip_protocol
   from_port         = each.value.ip_protocol == "-1" ? null : each.value.from_port != null ? each.value.from_port : each.value.port
   to_port           = each.value.ip_protocol == "-1" ? null : each.value.to_port != null ? each.value.to_port : each.value.port
@@ -28,7 +28,7 @@ resource "aws_vpc_security_group_egress_rule" "this" {
 
   security_group_id = aws_security_group.this.id
   description       = each.value.description != null ? each.value.description : "Allow ${each.key}"
-  cidr_ipv4         = each.value.cidr_ipv4 != null ? each.value.cidr_ipv4 : "tcp"
+  cidr_ipv4         = each.value.cidr_ipv4 != null ? each.value.cidr_ipv4 : var.vpc.cidr_block
   ip_protocol       = each.value.ip_protocol
   from_port         = each.value.ip_protocol == "-1" ? null : each.value.from_port != null ? each.value.from_port : each.value.port
   to_port           = each.value.ip_protocol == "-1" ? null : each.value.to_port != null ? each.value.to_port : each.value.port

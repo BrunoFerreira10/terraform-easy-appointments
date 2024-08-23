c4f85c32045bd44c5090093f69fc1d1bbe6cfb7c
BrunoFerreira10 <brunoferreira86dev@gmail.com>
2024-08-23 18:59:42 -0300

    Correcao para image builder usar o github

--- 2a-vpc_app/main.tf
+++ 2a-vpc_app/main.tf
@@ -12,6 +12,7 @@ module "vpc_app" {
           EPHEMERAL = { rule_number = 400, cidr_block = "0.0.0.0/0", from_port = 32768, to_port = 65535 }
         },
         egress = {
+          SSH_GIT   = { rule_number = 50, port = 22, cidr_block = "0.0.0.0/0" }, # GIT IMAGE BUILDER
           SSH       = { rule_number = 100, port = 22 },
           HTTP      = { rule_number = 200, cidr_block = "0.0.0.0/0", port = 80 },
           HTTPS     = { rule_number = 300, cidr_block = "0.0.0.0/0", port = 443 },

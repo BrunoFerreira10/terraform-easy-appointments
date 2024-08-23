25e2a8c8b99d8b903d708daf10cae001272ec20a
BrunoFerreira10 <brunoferreira86dev@gmail.com>
2024-08-22 22:19:34 -0300

    Corrigido private ip

--- modules/database/rds/generic-mysql/outputs.tf
+++ modules/database/rds/generic-mysql/outputs.tf
@@ -4,7 +4,7 @@ output "rds" {
     id                        = aws_db_instance.rds.id,
     arn                       = aws_db_instance.rds.arn,
     endpoint                  = split(":", aws_db_instance.rds.endpoint)[0],
-    private_ip                = data.aws_network_interface.rds_eni[0].private_ip
+    private_ip                = data.aws_network_interface.rds_eni.private_ip
     port                      = aws_db_instance.rds.port,
     db_name                   = aws_db_instance.rds.db_name
     db_username               = aws_db_instance.rds.username

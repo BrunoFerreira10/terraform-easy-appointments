ba771ea685225e8a6cef89f5dcf531ffd9f9510c
BrunoFerreira10 <brunoferreira86dev@gmail.com>
2024-08-23 08:17:31 -0300

    Depurando user-data

--- modules/compute/ec2/generic_elb/02_launch_template.tf
+++ modules/compute/ec2/generic_elb/02_launch_template.tf
@@ -1,11 +1,5 @@
-resource "aws_launch_template" "this" {
 
-  name                   = "launch_tpl_${var.shortname}"
-  update_default_version = true
-
-  image_id      = var.ami_image_id
-  instance_type = var.instance_type
-  key_name      = var.ec2_ssh_keypair_name
+locals {
   user_data = templatefile(
     "${path.module}/scripts/userdata.tftpl", {
       EFS_DNS_NAME       = var.efs.dns_name,
@@ -14,7 +8,7 @@ resource "aws_launch_template" "this" {
       DB_HOST            = var.rds.private_ip,
       DB_NAME            = var.rds.db_name,
       DB_USERNAME        = var.rds.db_username,
-      DB_PASSWORD        = base64encode(data.aws_ssm_parameter.db_password.value),
+      DB_PASSWORD        = data.aws_ssm_parameter.db_password.value,
       MEU_TESTE = templatefile(
         "${path.module}/scripts/teste.tftpl", {
           DOMAIN = "maooooes.com"
@@ -22,6 +16,40 @@ resource "aws_launch_template" "this" {
       )
     }
   )
+}
+resource "null_resource" "debug_user_data" {
+  provisioner "local-exec" {
+    command = "echo '${local.user_data}'"
+  }
+}
+
+resource "aws_launch_template" "this" {
+  depends_on = [ null_resource.debug_user_data ]
+  name                   = "launch_tpl_${var.shortname}"
+  update_default_version = true
+
+  image_id      = var.ami_image_id
+  instance_type = var.instance_type
+  key_name      = var.ec2_ssh_keypair_name
+
+  user_data = local.user_data
+
+  # user_data = templatefile(
+  #   "${path.module}/scripts/userdata.tftpl", {
+  #     EFS_DNS_NAME       = var.efs.dns_name,
+  #     APP_REPOSITORY_URL = var.app_repository_url,
+  #     DOMAIN             = var.domain,
+  #     DB_HOST            = var.rds.private_ip,
+  #     DB_NAME            = var.rds.db_name,
+  #     DB_USERNAME        = var.rds.db_username,
+  #     DB_PASSWORD        = data.aws_ssm_parameter.db_password.value,
+  #     MEU_TESTE = templatefile(
+  #       "${path.module}/scripts/teste.tftpl", {
+  #         DOMAIN = "maooooes.com"
+  #       }
+  #     )
+  #   }
+  # )
 
   vpc_security_group_ids = [
     module.sg_launch_tpl.security_group.id

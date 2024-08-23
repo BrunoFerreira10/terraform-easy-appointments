563b773fb852aa8a39c0fd66214141ae4f566fc5
BrunoFerreira10 <brunoferreira86dev@gmail.com>
2024-08-23 07:35:22 -0300

    Entradas ami_app e acm_app

--- 6a-elb_app/main.tf
+++ 6a-elb_app/main.tf
@@ -1,9 +1,9 @@
 module "elb_app" {
   source               = "../modules/compute/ec2/generic_elb"
-  acm_certificate      = module.data.projects.acm_ssl.certificate
+  acm_certificate      = module.data.projects.acm_app.certificate
   domain               = module.data.github_vars.rt53_domain
   ec2_ssh_keypair_name = module.data.github_vars.ec2_ssh_keypair_name
-  ami_image_id         = module.data.projects.app_ami.golden_image_id
+  ami_image_id         = module.data.projects.ami_app.golden_image_id
   shortname            = module.data.github_vars.general_tag_shortname
   app_repository_url   = module.data.github_vars.app_repository_url
   vpc                  = module.data.projects.vpc_app.vpc

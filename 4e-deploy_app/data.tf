333100404cacbfd5cf301cb1c108061b223d5cf2
BrunoFerreira10 <brunoferreira86dev@gmail.com>
2024-08-24 12:46:34 -0300

    Corrigida github connection

--- 4e-deploy_app/data.tf
+++ 4e-deploy_app/data.tf
@@ -2,7 +2,3 @@ module "data" {
   source = "../modules/data"
   requested_data = []
 }
-
-data "aws_codestarconnections_connection" "github_app_connection" {
-  name = "github_app_connection"
-}
--- modules/developer/tools/generic_code_deploy/data.tf
+++ modules/developer/tools/generic_code_deploy/data.tf
@@ -4,3 +4,7 @@ data "aws_ssm_parameter" "github_token" {
   name = "/github_secrets/github_token"
 }
 
+data "aws_codestarconnections_connection" "github_app_connection" {
+  name = "github_app_connection"
+}
+

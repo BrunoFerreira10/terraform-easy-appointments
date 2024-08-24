b2b95db50cd69adeb1e20e55606e188e01511bcc
BrunoFerreira10 <brunoferreira86dev@gmail.com>
2024-08-24 16:05:35 -0300

    Adicionado github_connection_name

--- 4e-deploy_app/main.tf
+++ 4e-deploy_app/main.tf
@@ -3,8 +3,9 @@ module "build_app" {
   app_repository_url_https = module.data.github_vars.app_repository_url_https
   codebuild_settings = {
     project_name = "EasyAppointments"
+    github_connection_name = module.data.github_vars.my_github_connection_name
   }
-  # github_connection = aws_codestarconnections_connection.github_app_connection
+  
   project_bucket_name = module.data.github_vars.general_project_bucket_name
   region              = module.data.github_vars.general_region
   shortname           = module.data.github_vars.general_tag_shortname

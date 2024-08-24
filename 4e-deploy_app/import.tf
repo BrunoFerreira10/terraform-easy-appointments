4872c198762b4098c325d22fd2c73ef07230e6e8
BrunoFerreira10 <brunoferreira86dev@gmail.com>
2024-08-24 11:20:50 -0300

    Testes conexao code connetions

--- 4e-deploy_app/import.tf
+++ 4e-deploy_app/import.tf
@@ -1,4 +1,4 @@
 import {
-  to = aws_codebuild_source_credential.github_app_connection
+  to = aws_codestarconnections_connection.github_app_connection
   id = "arn:aws:codeconnections:us-east-1:339712924273:connection/48875c7b-6fdb-45e6-9bb2-74bd01d296d9"
 }

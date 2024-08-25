ef265d88eaa3e671a975504404911a55ce80ab58
BrunoFerreira10 <brunoferreira86dev@gmail.com>
2024-08-25 12:53:41 -0300

    Depurando criação do arquivo zip

--- modules/developer/tools/generic_code_deploy/03_lambda.tf
+++ modules/developer/tools/generic_code_deploy/03_lambda.tf
@@ -1,16 +1,6 @@
-#Criação do arquivo ZIP contendo o código da função Lambda
-resource "local_file" "lambda_zip" {
-  filename = "lambda_function.zip"
-  content  = "${path.module}/scripts/lambda_function_local.py"
-
-  provisioner "local-exec" {
-    command = <<EOT
-      echo "hello local-exec 111" 
-      tree
-    EOT
-  }
-}
-
+## --------------------------------------------------------------------------------------------------------------------
+## Criação da função lambda via arquivo python.
+## --------------------------------------------------------------------------------------------------------------------
 data "archive_file" "lambda_zip" {
   depends_on = [ local_file.lambda_zip ]
   type        = "zip"
@@ -18,19 +8,7 @@ data "archive_file" "lambda_zip" {
   output_path = "lambda_function.zip"  
 }
 
-resource "null_resource" "name" {
-    depends_on = [ data.archive_file.lambda_zip ]
-    provisioner "local-exec" {
-    command = <<EOT
-      echo "hello local-exec 222" 
-      tree
-    EOT
-  }
-}
-
-# Criação da função Lambda
-resource "aws_lambda_function" "codedeploy_trigger_lambda" {
-  depends_on = [ null_resource.name ]
+resource "aws_lambda_function" "codedeploy_trigger_lambda" {  
   function_name = "CodeDeployTriggerLambda"
   role          = aws_iam_role.lambda_codedeploy_role.arn
   handler       = "lambda_function.lambda_handler"

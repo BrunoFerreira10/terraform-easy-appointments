8815c90aebf8d3f8ff3b613a61997141cdf0e111
BrunoFerreira10 <brunoferreira86dev@gmail.com>
2024-08-25 11:56:53 -0300

    Depurando criação do arquivo zip

--- modules/developer/tools/generic_code_deploy/03_lambda.tf
+++ modules/developer/tools/generic_code_deploy/03_lambda.tf
@@ -18,8 +18,8 @@ resource "aws_lambda_function" "codedeploy_trigger_lambda" {
   handler       = "lambda_function.lambda_handler"
   runtime       = "python3.9"
 
-  filename         = "lambda_function.zip"
-  source_code_hash = filebase64sha256("lambda_function.zip")
+  filename         = local.local_file.filename
+  source_code_hash = filebase64sha256(local.local_file.filename)
 
   environment {
     variables = {

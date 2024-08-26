6225c5809914678ab2f960225a55923ad3f2ec31
BrunoFerreira10 <brunoferreira86dev@gmail.com>
2024-08-26 12:25:05 -0300

    Revisao bastion host

--- modules/compute/ec2/bastion/variables.tf
+++ modules/compute/ec2/bastion/variables.tf
@@ -21,25 +21,3 @@ variable "sg_bastion_rules" {
   description = "Rules for bastion host security group"
   type        = any
 }
-
-# variable "sg_bastion_rules" {
-#   description = "Rules for bastion host security group"
-#   type        = object({
-#     ingress = map(object({
-#       description = optional(string),
-#       cidr_ipv4   = optional(string),
-#       ip_protocol = optional(string, "tcp"),
-#       from_port   = optional(number),
-#       to_port     = optional(number),
-#       port        = optional(number)
-#     })),
-#     egress = map(object({
-#       description = optional(string),
-#       cidr_ipv4   = optional(string),
-#       ip_protocol = optional(string, "tcp"),
-#       from_port   = optional(number),
-#       to_port     = optional(number),
-#       port        = optional(number)
-#     }))
-#   })
-# }

f43deeccc2274c08cb3d8b42d097e386aa38e32b
BrunoFerreira10 <brunoferreira86dev@gmail.com>
2024-08-26 16:18:05 -0300

    Adicionada politica IAM para codedeploy-agent endpoint

--- modules/compute/ec2/generic_elb/02_iam.tf
+++ modules/compute/ec2/generic_elb/02_iam.tf
@@ -2,7 +2,31 @@
 ## Launch template instances policies and role.
 ## --------------------------------------------------------------------------------------------------------------------
 ## Policies
+resource "aws_iam_policy" "codedeploy_vcp_endpoint" {
+  name        = "CodeDeployVpcEndpoint-${var.shortname}"
+  path        = "/TerraformManaged/"
+  description = "Politica para EC2 usar VPC endpoint do agente do CodeDeploy"
 
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action : [
+          "codedeploy-commands-secure:GetDeploymentSpecification",
+          "codedeploy-commands-secure:PollHostCommand",
+          "codedeploy-commands-secure:PutHostCommandAcknowledgement",
+          "codedeploy-commands-secure:PutHostCommandComplete"
+        ],
+        Effect : "Allow",
+        Resource : "*"
+      }
+    ]
+  })
+
+  tags = {
+    Name = "CodeDeployVpcEndpoint-${var.shortname}"
+  }
+}
 
 ## Role
 resource "aws_iam_role" "launch_tpl" {
@@ -28,12 +52,16 @@ resource "aws_iam_role" "launch_tpl" {
   }
 }
 
+resource "aws_iam_role_policy_attachment" "codedeploy_vcp_endpoint_to_launch_tpl" {
+  policy_arn = aws_iam_policy.codedeploy_vcp_endpoint.arn
+  role       = aws_iam_role.launch_tpl.name
+}
+
 # Necessária para download do build.zip no s3 durante o deploy.
 resource "aws_iam_role_policy_attachment" "s3_read_to_launch_tpl" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonS3ReadOnlyAccess"
   role       = aws_iam_role.launch_tpl.name
 }
-
 
 resource "aws_iam_role_policy_attachment" "ssm_managed_to_launch_tpl" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore" # TODO - Verificar se ainda precisa.

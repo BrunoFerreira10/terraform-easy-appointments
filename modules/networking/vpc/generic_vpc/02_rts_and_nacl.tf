1eada330d730e0ecead2e884122558d5211c7d3c
BrunoFerreira10 <brunoferreira86dev@gmail.com>
2024-08-26 15:22:34 -0300

    Politica para code deploy vcp endpoint

--- modules/networking/vpc/generic_vpc/02_rts_and_nacl.tf
+++ modules/networking/vpc/generic_vpc/02_rts_and_nacl.tf
@@ -25,6 +25,11 @@ resource "aws_route_table" "public" {
 resource "aws_route_table" "private" {
   vpc_id = aws_vpc.app.id
 
+    route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.app.id
+  }
+
   tags = {
     Name = "rt_private_app_${var.shortname}"
   }

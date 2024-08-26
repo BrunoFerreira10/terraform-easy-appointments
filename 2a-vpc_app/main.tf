7a367bd1cfabb6a89d36573dce51a8d34c974ecf
BrunoFerreira10 <brunoferreira86dev@gmail.com>
2024-08-26 15:04:13 -0300

    Politica para code deploy vcp endpoint

--- 2a-vpc_app/main.tf
+++ 2a-vpc_app/main.tf
@@ -23,7 +23,8 @@ module "vpc_app" {
       private = {
         ingress = {
           #SSH       = { rule_number = 100, port = 22 }, # BATION HOST
-          HTTP      = { rule_number = 200, port = 80 },
+          HTTP           = { rule_number = 102, port = 80, cidr_block = "0.0.0.0/0" },                        # USER DATA
+          HTTPS          = { rule_number = 202, port = 443, cidr_block = "0.0.0.0/0" },                        # USER DATA
           #EPHEMERAL_TEMP = { rule_number = 400, from_port = 1024, to_port = 65535, cidr_block = "0.0.0.0/0" } # USER DATA
           EPHEMERAL = { rule_number = 500, from_port = 1024, to_port = 65535 } # BASTION HOST
           EPHEMERAL_NAT = { rule_number = 400, cidr_block = "0.0.0.0/0", from_port = 1024, to_port = 65535 }

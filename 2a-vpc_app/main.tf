e4fc180c8af6d93536b47edeb0bc573862930943
BrunoFerreira10 <brunoferreira86dev@gmail.com>
2024-08-26 11:40:26 -0300

    Atualizadas nacls

--- 2a-vpc_app/main.tf
+++ 2a-vpc_app/main.tf
@@ -6,29 +6,29 @@ module "vpc_app" {
     nacl_rules = {
       public = {
         ingress = {
-          SSH       = { rule_number = 100, cidr_block = "${data.aws_ssm_parameter.my_ip.value}/32", port = 22 },
+          # SSH       = { rule_number = 100, cidr_block = "${data.aws_ssm_parameter.my_ip.value}/32", port = 22 }, # BASTION HOST
           HTTP      = { rule_number = 200, cidr_block = "0.0.0.0/0", port = 80 },
           HTTPS     = { rule_number = 300, cidr_block = "0.0.0.0/0", port = 443 },
           EPHEMERAL = { rule_number = 400, cidr_block = "0.0.0.0/0", from_port = 1024, to_port = 65535 }
         },
         egress = {
-          SSH_GIT   = { rule_number = 50, port = 22, cidr_block = "0.0.0.0/0" }, # GIT IMAGE BUILDER
-          SSH       = { rule_number = 100, port = 22 },
-          HTTP      = { rule_number = 200, cidr_block = "0.0.0.0/0", port = 80 },
-          HTTPS     = { rule_number = 300, cidr_block = "0.0.0.0/0", port = 443 },
+          #SSH_GIT   = { rule_number = 50, port = 22, cidr_block = "0.0.0.0/0" }, # GIT IMAGE BUILDER
+          # SSH       = { rule_number = 100, port = 22 }, # BASTION HOST
+          #HTTP      = { rule_number = 200, cidr_block = "0.0.0.0/0", port = 80 }, # BATION HOST
+          #HTTPS     = { rule_number = 300, cidr_block = "0.0.0.0/0", port = 443 }, # BATION HOST
           EPHEMERAL = { rule_number = 500, cidr_block = "0.0.0.0/0", from_port = 1024, to_port = 65535 }
         }
       },
       private = {
         ingress = {
-          SSH       = { rule_number = 100, port = 22 },
+          #SSH       = { rule_number = 100, port = 22 }, # BATION HOST
           HTTP      = { rule_number = 200, port = 80 },
-          EPHEMERAL_TEMP = { rule_number = 400, from_port = 1024, to_port = 65535, cidr_block = "0.0.0.0/0" } # USER DATA
-          EPHEMERAL = { rule_number = 500, from_port = 1024, to_port = 65535 }
+          #EPHEMERAL_TEMP = { rule_number = 400, from_port = 1024, to_port = 65535, cidr_block = "0.0.0.0/0" } # USER DATA
+          #EPHEMERAL = { rule_number = 500, from_port = 1024, to_port = 65535 } # BASTION HOST
         },
         egress = {
-          SSH            = { rule_number = 102, port = 22, cidr_block = "0.0.0.0/0" },                         # USER DATA
-          HTTPS          = { rule_number = 202, port = 443, cidr_block = "0.0.0.0/0" },                        # USER DATA
+          #SSH            = { rule_number = 102, port = 22, cidr_block = "0.0.0.0/0" },                         # USER DATA
+          #HTTPS          = { rule_number = 202, port = 443, cidr_block = "0.0.0.0/0" },                        # USER DATA
           EPHEMERAL      = { rule_number = 402, from_port = 1024, to_port = 65535 }
         }
       }

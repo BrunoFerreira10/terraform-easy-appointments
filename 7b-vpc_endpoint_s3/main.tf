df22d526c1b9afaa1dcb98405efdf89e71f13ef1
BrunoFerreira10 <brunoferreira86dev@gmail.com>
2024-08-30 19:42:54 -0300

    Limpeza

--- 7b-vpc_endpoint_s3/main.tf
+++
@@ -1,6 +0,0 @@
-module "deploy_app" {
-  source                   = "../modules/networking/vpc/generic_vpc_endpoint_gateway"
-  region = module.data.github_vars.general_region
-  shortname = module.data.github_vars.general_tag_shortname
-  vpc = module.data.projects.vpc_app.vpc
-}

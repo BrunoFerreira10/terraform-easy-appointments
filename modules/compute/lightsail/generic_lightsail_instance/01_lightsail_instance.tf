97c1b8582ddde05dadbf6c0e386136a96020e420
BrunoFerreira10 <brunoferreira86dev@gmail.com>
2024-08-30 14:16:36 -0300

    Automação certificado

--- modules/compute/lightsail/generic_lightsail_instance/01_lightsail_instance.tf
+++ modules/compute/lightsail/generic_lightsail_instance/01_lightsail_instance.tf
@@ -7,21 +7,22 @@ resource "aws_lightsail_instance" "this" {
   key_pair_name     = var.ec2_ssh_keypair_name
 
   user_data = templatefile("${path.module}/scripts/userdata.sh.tpl", {
-    REGION            = var.region
-    IAM_USER_ARN      = aws_iam_user.lightsail_instance.arn
-    ACCESS_KEY_ID     = aws_iam_access_key.lightsail_instance.id
-    SECRET_ACCESS_KEY = aws_iam_access_key.lightsail_instance.secret
+    REGION                = var.region
+    IAM_USER_ARN          = aws_iam_user.lightsail_instance.arn
+    AWS_ACCESS_KEY_ID     = aws_iam_access_key.lightsail_instance.id
+    AWS_SECRET_ACCESS_KEY = aws_iam_access_key.lightsail_instance.secret
+    INSTANCE_NAME         = "ltsail_${var.shortname}"
     CODEDEPLOY_ONPREMISES_YML = templatefile("${path.module}/scripts/codedeploy.onpremises.yml.tpl", {
-      REGION            = var.region
-      DOMAIN            = var.domain
-      ACCESS_KEY_ID     = aws_iam_access_key.lightsail_instance.id
-      SECRET_ACCESS_KEY = aws_iam_access_key.lightsail_instance.secret
-      IAM_USER_ARN      = aws_iam_user.lightsail_instance.arn
+      REGION                = var.region
+      DOMAIN                = var.domain
+      AWS_ACCESS_KEY_ID     = aws_iam_access_key.lightsail_instance.id
+      AWS_SECRET_ACCESS_KEY = aws_iam_access_key.lightsail_instance.secret
+      IAM_USER_ARN          = aws_iam_user.lightsail_instance.arn
     })
     SETUP_CONFIG_PHP_SH = templatefile("${path.module}/scripts/setup_config_php.sh.tpl", {
-      DOMAIN            = var.domain
+      DOMAIN = var.domain
     })
-    BNCERT_TOOL_EXP   = templatefile("${path.module}/scripts/bncert_tool.exp.tpl", {
+    BNCERT_TOOL_EXP = templatefile("${path.module}/scripts/bncert_tool.exp.tpl", {
     })
   })
 
